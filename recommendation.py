96dae19f05eaf900e447b3c767af666fdf1a50e4
varshitar0408 <varshitar0408@gmail.com>
2026-05-11 22:04:44 +0530

    recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,6 +1,4 @@
-# Simple Collaborative Filtering Recommendation System
 
-# User movie ratings
 users = {
     "Rahul": {
         "KGF": 5,
@@ -23,29 +21,24 @@ users = {
 
 print("=== Movie Recommendation System ===")
 
-# Taking user name
 name = input("Enter user name (Rahul/Anu/Ravi): ")
 
-# Recommendation logic
 if name in users:
 
     watched = users[name]
 
     recommendations = []
 
-    # Compare with other users
     for other_user in users:
 
         if other_user != name:
 
             for movie in users[other_user]:
 
-                # Recommend unseen movies
                 if movie not in watched:
 
                     recommendations.append(movie)
 
-    # Remove duplicates
     recommendations = list(set(recommendations))
 
     print("\nRecommended Movies:")
