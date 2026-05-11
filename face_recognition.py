b138c77c9907d6f4842d8f06ab8489d8a46f77ba
varshitar0408 <varshitar0408@gmail.com>
2026-05-11 21:13:54 +0530

    face_recognition.py
    TASK 5
    FACE DETECTION AND
    RECOGNITION

--- face_recognition.py
+++ face_recognition.py
@@ -1,23 +1,18 @@
 import cv2
 
-# Load pre-trained face detector
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades +
     'haarcascade_frontalface_default.xml'
 )
 
-# Start webcam
 cap = cv2.VideoCapture(0)
 
 while True:
 
-    # Read frame
     ret, frame = cap.read()
 
-    # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces
     faces = face_cascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -25,7 +20,6 @@ while True:
         minSize=(30, 30)
     )
 
-    # Draw rectangles
     for (x, y, w, h) in faces:
 
         cv2.rectangle(
@@ -46,13 +40,10 @@ while True:
             2
         )
 
-    # Show output
     cv2.imshow("Face Detection System", frame)
 
-    # Press Q to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release webcam
 cap.release()
 cv2.destroyAllWindows()
